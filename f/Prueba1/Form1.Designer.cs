865a50b823a6256e5f887717f226883c2b5b5ca9
Camila <camiigamesho@gmail.com>
2021-12-16 23:38:33 +0100

    def

---
+++ f/Prueba1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Prueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.r = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.r)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // r
+            // 
+            this.r.Enabled = true;
+            this.r.Location = new System.Drawing.Point(28, 13);
+            this.r.Name = "r";
+            this.r.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("r.OcxState")));
+            this.r.Size = new System.Drawing.Size(513, 50);
+            this.r.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Prueba1.Properties.Resources.c_mercadolibre_250320_r;
+            this.pictureBox1.Location = new System.Drawing.Point(145, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 139);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(529, 277);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.r);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.r)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer r;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ f/Prueba1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba1
+{
+    public partial class Form1 : Form
+    {
+
+        bool star = false; /// Variable bool,guarda false,true
+        string[] Archivos; //array donde se depositan todos los archivos encontrados
+        string[] Rutas; //array
+        /// </summary>
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog Carpeta = new OpenFileDialog();///muestra el sistema operativo de windows cuando deseas seleccionar un archivo.
+
+            Carpeta.Multiselect = true; //Permite seleccionar varios archivos almismo tiempo :)
+
+            if (Carpeta.ShowDialog() == DialogResult.OK) //Si se mostro el openfiledialog y si le dieron elboton okay
+            {
+
+                Archivos = Carpeta.SafeFileNames; //se almacena todo los archivos
+                Rutas = Carpeta.FileNames; //Guarda ruta de los archivos
+
+                foreach (var Archivos in   Archivos)//ciclo foreach par leer cada uno de los archivos que se adjuntn
+                {
+
+
+                    listBox1.Items.Add(Archivos); //se crea una lista de acuerdo a los archivos encontrados
+
+                }
+                r.URL = Rutas[0];//La ruta del archivo inicial
+                listBox1.SelectedIndex = 0; //selecciona 
+
+
+
+            }
+
+        }
+    }
+}
